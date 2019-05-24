c17acf7b10e916e83e11c6d8b187b4c024cc6422
Shankar Kumar <Shankar@vartulz.com>
2019-05-24 18:54:34 +0530

    commit

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "HelloVaporSwift",
+    name: "SwiftVeporSQL",
     products: [
-        .library(name: "HelloVaporSwift", targets: ["App"]),
+        .library(name: "SwiftVeporSQL", targets: ["App"]),
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
@@ -13,11 +13,10 @@ let package = Package(
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         // 🐬 Pure Swift MySQL client built on non-blocking, event-driven sockets.
-        .package(url: "https://github.com/vapor/mysql.git", from: "3.0.0")
-
+        .package(url: "https://github.com/vapor/mysql.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["MySQL","FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "MySQL", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
