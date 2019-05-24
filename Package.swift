a993a0cf296a2202ec03d7602c5f61cb8a86b4bb
Shankar Kumar <Shankar@vartulz.com>
2019-05-24 18:07:36 +0530

    first commit

--- Package.swift
+++ Package.swift
@@ -11,10 +11,13 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+        // 🐬 Pure Swift MySQL client built on non-blocking, event-driven sockets.
+        .package(url: "https://github.com/vapor/mysql.git", from: "3.0.0")
+
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["MySQL","FluentSQLite", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,9 +12,23 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
 
+    router.get("sql") { req in
+        return req.withPooledConnection(to: .mysql) { conn in
+            return conn.raw("SELECT @@version as version")
+                .all(decoding: MySQLVersion.self)
+            }.map { rows in
+                return rows[0].version
+        }
+    }
+
+    
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
 }
+struct MySQLVersion: Codable {
+    let version: String
+}
+
